c1099308bc631971b0e138a081fb076f3b14270e
Anton Patsev <patsev.anton@gmail.com>
2024-03-03 12:01:56 +0600

    freeipa-smallstep

--- inventory_yml.tpl
+++ inventory_yml.tpl
@@ -1,15 +1,13 @@
 all:
   children:
-    freipa-ca:
+    freeipa-smallstep:
       hosts:
-        "freeipa":
-          ansible_host: "${freeipa_public_ip}"
-        "step_ca":
-          ansible_host: "${smallstep_public_ip}"
+        "freeipa-smallstep":
+          ansible_host: "${public_ip}"
   vars:
     ansible_user:  ${ssh_user}
     ansible_ssh_private_key_file: ~/.ssh/id_rsa
     freeipa_password: "${freeipa_password}"
-    freeipa_fqdn: "${freeipa_internal_ip}.${freeipa_domain}"
+    freeipa_fqdn: "${internal_ip}.${freeipa_domain}"
     freeipa_domain: "${freeipa_domain}"
     ssh_user: "${ssh_user}"
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "smallstep" {
+module "freeipa-smallstep" {
   source        = "git::https://github.com/patsevanton/terraform-yandex-compute.git?ref=v1.23.0"
   image_family  = var.family_images_linux
   subnet_id     = data.yandex_vpc_subnet.default-ru-central1-b.id
@@ -13,29 +13,12 @@ module "smallstep" {
   nat_ip_address = var.nat_ip_address
 }
 
-module "freeipa" {
-  source        = "git::https://github.com/patsevanton/terraform-yandex-compute.git?ref=v1.23.0"
-  image_family  = var.family_images_linux
-  subnet_id     = data.yandex_vpc_subnet.default-ru-central1-b.id
-  zone          = data.yandex_vpc_subnet.default-ru-central1-b.zone
-  name          = "freeipa"
-  hostname      = "freeipa"
-  memory        = "4"
-  is_nat        = true
-  preemptible   = true
-  core_fraction = 100
-  user          = var.ssh_user
-  nat_ip_address = var.nat_ip_address
-}
-
 resource "local_file" "inventory_yml" {
   content = templatefile("inventory_yml.tpl",
     {
       ssh_user               = var.ssh_user
-      freeipa_public_ip      = module.freeipa.external_ip[0]
-      freeipa_internal_ip    = module.freeipa.internal_ip[0]
-      smallstep_public_ip    = module.smallstep.external_ip[0]
-      smallstep_internal_ip  = module.smallstep.internal_ip[0]
+      public_ip      = module.freeipa-smallstep.external_ip[0]
+      internal_ip    = module.freeipa-smallstep.internal_ip[0]
       freeipa_password       = var.freeipa_password
 #      freeipa_fqdn          = var.freeipa_fqdn
       freeipa_domain         = var.freeipa_domain
@@ -46,22 +29,12 @@ resource "local_file" "inventory_yml" {
 }
 
 
-output "smallstep_public_ip" {
-  description = "Public IP address smallstep"
-  value       = module.smallstep.external_ip[0]
+output "public_ip" {
+  description = "Public IP address freeipa-smallstep"
+  value       = module.freeipa-smallstep.external_ip[0]
 }
 
-output "smallstep_internal_ip" {
-  description = "Internal IP address smallstep"
-  value       = module.smallstep.internal_ip[0]
-}
-
-output "freeipa_public_ip" {
-  description = "Public IP address FreeIPA"
-  value       = module.freeipa.external_ip[0]
-}
-
-output "freeipa_internal_ip" {
-  description = "Internal IP address FreeIPA"
-  value       = module.freeipa.internal_ip[0]
+output "internal_ip" {
+  description = "Internal IP address freeipa-smallstep"
+  value       = module.freeipa-smallstep.internal_ip[0]
 }
